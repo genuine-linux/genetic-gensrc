53be08824e9375184d117950ff13e37898c207d6
Antonio Cao <antoniocao000@gmail.com>
2020-02-27 10:08:32 +0100

    2020 Update

--- texlive/texlive/files/etc/profile.d/texlive.sh
+++ texlive/texlive/files/etc/profile.d/texlive.sh
@@ -1,5 +1,9 @@
 # Begin /etc/profile.d/texlive.sh
 
-pathprepend /opt/texlive/bin           PATH
+TEXARCH=$(uname -m | sed -e 's/i.86/i386/' -e 's/$/-linux/') &&
+
+pathappend /opt/texlive/2019/texmf-dist/doc/man  MANPATH
+pathappend /opt/texlive/2019/texmf-dist/doc/info INFOPATH
+pathappend /opt/texlive/2019/bin/$TEXARCH
 
 # End /etc/profile.d/texlive.sh
